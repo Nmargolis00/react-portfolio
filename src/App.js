b0a65baa6a3eeb31028b85f358ed9a4e2b5f7c4f
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-05-10 11:32:54 -0600

    Navbar working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
-// import AboutMe from './components/AboutMe/AboutMe';
+
 
 function App() {
   return (
