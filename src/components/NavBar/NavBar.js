bd9c39f0d4245a4dbca64dac48bd1e5eeaf2bbd0
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-05-10 11:16:34 -0600

    added some CSS stlying

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,29 +2,21 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBar.css';
+
 
 function NavBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Nate Margolis</Navbar.Brand>
+        <Navbar.Brand className="navBar" href="#home">Nate Margolis</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">About Me</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link className="navBar" href="/">About Me</Nav.Link>
+            <Nav.Link className="navBar" href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link className="navBar" href="/contact">Contact</Nav.Link>
+            <Nav.Link className="navBar" href="https://drive.google.com/file/d/1nWWyI8_7VuBENfZKHbFrhMQMzhikVRio/view?usp=sharing" target="_blank">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
