3e8d13bc3a74e01431081e37c5235bd60f546f5c
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-05-12 10:50:16 -0600

    added in contact information box

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import "./contact.css";
 
 const FORM_ENDPOINT = ""; // TODO - fill on the later step
 
-const ContactForm = () => {
+const Contact = () => {
   const [submitted, setSubmitted] = useState(false);
   const handleSubmit = () => {
     setTimeout(() => {
@@ -14,19 +15,21 @@ const ContactForm = () => {
     return (
       <>
         <div className="text-2xl">Thank you!</div>
-        <div className="text-md">We'll be in touch soon.</div>
+        <div className="text-md">I'll be in touch soon.</div>
       </>
     );
   }
 
   return (
+    <div className="contact-container">
+        <h1 className="contact-title">Let's get in touch!</h1>
     <form
       action={FORM_ENDPOINT}
       onSubmit={handleSubmit}
       method="POST"
       target="_blank"
     >
-      <div className="mb-3 pt-0">
+      <div className="input-field mb-3 pt-0">
         <input
           type="text"
           placeholder="Your name"
@@ -35,7 +38,7 @@ const ContactForm = () => {
           required
         />
       </div>
-      <div className="mb-3 pt-0">
+      <div className="input-field mb-3 pt-0">
         <input
           type="email"
           placeholder="Email"
@@ -44,7 +47,7 @@ const ContactForm = () => {
           required
         />
       </div>
-      <div className="mb-3 pt-0">
+      <div className="input-field mb-3 pt-0">
         <textarea
           placeholder="Your message"
           name="message"
@@ -61,7 +64,8 @@ const ContactForm = () => {
         </button>
       </div>
     </form>
+    </div>
   );
 };
 
-export default ContactForm;
+export default Contact;
