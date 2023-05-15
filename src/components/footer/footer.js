85b1550271e5f9deffd995bba4ba394105583c3c
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-05-15 12:51:34 -0600

    fixed styling

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,49 +1,61 @@
 import * as React from "react";
 import "./footer.css";
-import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+// import Box from "@mui/material/Box";
+// import BottomNavigation from "@mui/material/BottomNavigation";
+// import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Footer() {
-  //   const [value, setValue] = React.useState(0);
-
   return (
-    <Box className="footer-div" >
-      <BottomNavigation
-        showLabels
-        // value={value}
-        onChange={(event, newValue) => {
-          // setValue(newValue);
-        }}
+    <div className="footer-div">
+      <div className="icon-wrapper">
+      <a href="https://github.com/Nmargolis00" target="_blank" rel="noreferrer">
+        <GitHubIcon className="icon" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/nate-margolis-9105b019/"
+        target="_blank"
+        rel="noreferrer"
       >
-        <a
-          href="https://github.com/Nmargolis00"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <BottomNavigationAction
-            className="icon"
-            label="GitHub"
-            icon={<GitHubIcon />}
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/nate-margolis-9105b019/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <BottomNavigationAction
-            className="icon"
-            label="LinkedIn"
-            icon={<LinkedInIcon />}
-          />
-        </a>
-      </BottomNavigation>
-    
+        <LinkedInIcon className="icon" />
+      </a>
+      </div>
       <h6 className="copy-right">&copy; 2023 Nate Margolis</h6>
-  
-    </Box>
+    </div>
   );
 }
+//   const [value, setValue] = React.useState(0);
+
+//   return (
+//     <div className="footer-div" >
+//       <BottomNavigation className="footer-box">
+//         <a
+//           href="https://github.com/Nmargolis00"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <BottomNavigationAction
+//             className="icon"
+//             label="GitHub"
+//             icon={<GitHubIcon />}
+//           />
+//         </a>
+//         <a
+//           href="https://www.linkedin.com/in/nate-margolis-9105b019/"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <BottomNavigationAction
+//             className="icon"
+//             label="LinkedIn"
+//             icon={<LinkedInIcon />}
+//           />
+//         </a>
+//       </BottomNavigation>
+
+//       <h6 className="copy-right">&copy; 2023 Nate Margolis</h6>
+
+//     </div>
+//   );
+// }
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,7 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './portfolio.css';
 //add in the images for the projects
-import Fog from './images/fog.jpg'
+import fog from './images/fog.jpg';
+import concert from './images/concert.jpg';
+import imageAi from './images/imageAi.jpg';
+import text from './images/text.jpg';
+import passwordImage from './images/passwordImage.jpg';
+import planner from './images/planner.jpg';
 
 
 const projects = [
@@ -10,55 +16,57 @@ const projects = [
         description: "This is a concert finder app that allows you to search for concerts by artist, venue, or city. It also allows you to save concerts to your profile.",
         link: "https://ionscion.github.io/miniature-garbanzo",
         repo: "https://github.com/ionscion/miniature-garbanzo",
-        // image:
+        image: concert
     },
     {
         name: "Image AI",
         description: "This is an Image AI site that allows you to create an account, a new image with your custom search criteria and then save it to your profile.",
         link: "https://imageai.herokuapp.com/",
         repo: "https://github.com/Nmargolis00/image-AI",
+        image: imageAi
     },
     {
         name: "Weather Dashboard",
         description: "This is a weather tracker site that allows you to search for 5-day weather by city and state.",
         link: "https://nmargolis00.github.io/Weather-Tracker/",
         repo: "https://github.com/Nmargolis00/Weather-Tracker",
-        image: {Fog}
+        image: fog
     },
     {
         name: "PWA App",
         description: "This is a note taking app that allows you to take notes on and offline",
         link: "https://sheltered-lowlands-74185.herokuapp.com/",
         repo: "https://github.com/Nmargolis00/pwa-text-editor",
+        image: text
     },
     {
         name: "Password Generator",
         description: "This is a password generator site, need a new password? I got you covered.",
         link: "https://nmargolis00.github.io/password-generator/",
         repo: "https://github.com/Nmargolis00/password-generator",
+        image: passwordImage
     },
     {
         name: "Day Planner",
         description: "This is a day planning site. to help you plan your day and block time on your calendar.",
         link: "https://nmargolis00.github.io/Day-Planner/",
         repo: "https://github.com/Nmargolis00/Day-Planner",
+        image: planner
     },
 ]
 
+
+
 const Portfolio = () => {
     return (
-      <div className="mt-12">
-        <h3 className="text-2xl font-bold text-gray-800 mb-6 text-center">
-          Portfolio
-        </h3>
-  
+      <div className="main-div mt-12">
         <div
-          className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+          className="grid grid-cols-1 gap-14 sm:grid-cols-2 lg:grid-cols-3"
           style={{ maxHeight: 'calc(100vh - 150px)', overflowY: 'auto' }}
         >
           {projects.map((project, index) => (
             <Card key={index} style={{ width: '18rem' }}>
-              <img className="w-full h-60 object-cover" src={projects.image} alt={projects.name} />
+              <Card.Img className="w-full h-60 object-cover" src={projects.image} alt={projects.name} />
               <Card.Body>
                 <Card.Title>{project.name}</Card.Title>
                 <Card.Text>{project.description}</Card.Text>
@@ -68,7 +76,7 @@ const Portfolio = () => {
                   rel="noreferrer"
                   className="site-btn"
                 >
-                  <Button variant="primary">Check out the site</Button>
+                  <Button className= "btn" variant="primary">Check out the site</Button>
                 </a>
                 <a
                   href={project.repo}
@@ -76,7 +84,7 @@ const Portfolio = () => {
                   rel="noreferrer"
                   className="site-btn"
                 >
-                  <Button variant="primary">See the Repo</Button>
+                  <Button className= "btn" variant="primary">See the Repo</Button>
                 </a>
               </Card.Body>
             </Card>
